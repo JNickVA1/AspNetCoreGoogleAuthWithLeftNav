acd13bfc805d23110cfdb85490a1d91b28fe97ad
Jim Nicholson <nicholsonsoftware@hotmail.com>
2018-12-03 04:32:56 -0500

    Initial commit

---
+++ AspNetCoreGoogleAuthWithLeftNav/Pages/Profile.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreGoogleAuthWithLeftNav.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public async Task OnGet(string returnUrl = "/")
+        {
+	        await HttpContext.ChallengeAsync("Google", new AuthenticationProperties() { RedirectUri = System.Net.WebUtility.UrlEncode(returnUrl) });
+        }
+		public async Task OnPostLogin(string returnUrl = "/")
+	    {
+		    await HttpContext.ChallengeAsync("Google", new AuthenticationProperties() { RedirectUri = returnUrl });
+	    }
+
+	}
+}
--- AspNetCoreGoogleAuthWithLeftNav/Startup.cs
+++ AspNetCoreGoogleAuthWithLeftNav/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,8 +32,21 @@ namespace AspNetCoreGoogleAuthWithLeftNav
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
-
+			//
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			//
+			services.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+					options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				})
+				.AddCookie()
+				.AddGoogle("Google", options =>
+				{
+					options.ClientId = Configuration["Authentication:Google:ClientId"];
+					options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+				});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +64,7 @@ namespace AspNetCoreGoogleAuthWithLeftNav
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
+			app.UseAuthentication();
 			app.UseCookiePolicy();
 
 			app.UseMvc();
